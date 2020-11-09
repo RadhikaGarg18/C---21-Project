b39f9821ebeea043151fd728c83c969a08de26d1
RadhikaGarg18 <72866344+RadhikaGarg18@users.noreply.github.com>
2020-11-09 16:17:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,10 +3,10 @@ var speed, weight,thickness;
 
 function setup (){
     createCanvas(1600,400);
-    speed = random(55,90);
-    weight = random(400,1500);
+    speed = random(223,321);
+    weight = random(30,52);
     thickness = random(22,83)
-    bullet = createSprite(50,200,70,2);
+    bullet = createSprite(50,200,70,20);
     bullet.velocityX = speed;
     bullet.shapeColor = "white";
     wall = createSprite(1200,200,thickness,200);
